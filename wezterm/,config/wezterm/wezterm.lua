5dcd2f72186f72a9c4033cbef9f455f4786969e0
Samarth <teslawebber968@gmail.com>
2023-10-06 17:34:18 +0530

    Delete wezterm/,config/wezterm/wezterm.lua

--- wezterm/,config/wezterm/wezterm.lua
+++
@@ -1,6 +0,0 @@
-local wezterm = require 'wezterm'
-local config = {}
-config.font = wezterm.font 'Cascadia Code'
-config.font_size = 14
-config.color_scheme = 'Solarized Dark Higher Contrast (Gogh)'
-return config
