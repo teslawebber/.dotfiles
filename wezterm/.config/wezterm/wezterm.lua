d55a6706c31cf0f4c1cc1f5f719092255a1a823a
Samarth <teslawebber968@gmail.com>
2023-10-08 17:54:23 +0530

    Update wezterm.lua with color schemes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,4 +3,6 @@ local config = {}
 config.font = wezterm.font 'Cascadia Code'
 config.font_size = 14
 config.color_scheme = 'Solarized Dark Higher Contrast (Gogh)'
+-- config.color_scheme = 'Cobalt 2 (Gogh)'
+-- config.color_scheme = 'Dark Pastel'
 return config
